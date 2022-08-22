dbe7acc9fe43d47193c6103b3b3a94637705451b
afmnobre <lucas_guitar1987@hotmail.com>
2022-08-22 00:34:45 -0300

    Description:
    This time no story, no theory. The examples below show you how to write function accum:
    
    Examples:
    accum("abcd") -> "A-Bb-Ccc-Dddd"
    accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
    accum("cwAt") -> "C-Ww-Aaa-Tttt"
    
    The parameter of accum is a string which includes only letters from a..z and A..Z.

---
+++ exercicios/exercicio4.php
@@ -0,0 +1,53 @@
+<?php
+function accum($s) {
+    $word = str_split($s);
+    $cont1 = 0;
+    $cont2 = 0;
+    $show = 0;
+    $num = 0;
+    $trace = "";
+    $result = "";
+    $final = "";
+    foreach($word as $letter){
+            $cont1 ++;
+            $num = $cont1;
+            while($num > 0){
+                $cont2++;
+                if($cont2 == 1){
+                    if($cont1 > 1){ $trace = "-";}
+                    $result .= $trace.strtoupper($letter);
+                }else{
+                    $result .= strtolower($letter);
+                }
+            $num --;
+            
+            }
+            $cont2 = 0;
+}
+$final .= $result;
+return $final; 
+
+    
+
+
+
+
+
+
+
+
+
+}
+
+       
+
+
+
+
+
+
+
+echo accum("ZpglnRxqenU")."<p>";
+echo accum("NyffsGeyylB")."<p>";
+echo accum("MjtkuBovqrU")."<p>";
+echo accum("EvidjUnokmM")."<pr>";
