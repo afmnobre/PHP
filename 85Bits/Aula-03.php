d0ccdbf1380ce1535fed4d4ff3f6d17b313650f0
afmnobre <lucas_guitar1987@hotmail.com>
2022-10-09 16:45:37 -0300

    modified:   85Bits/Aula-03.php

--- 85Bits/Aula-03.php
+++ 85Bits/Aula-03.php
@@ -42,7 +42,7 @@ echo "Resultado: Meu nome é João!<p>";
 
 ##################################################################################################
 echo "<hr>";
-echo "<h4>2. Criando uma função com mais de um Parametro</h4>";
+echo "<h4>3. Criando uma função com mais de um Parametro</h4>";
 
 echo "<strong>Exemplo:</strong><p>";
 
@@ -59,7 +59,7 @@ echo "Resultado: Meu nome é João da Silva Sauro!<p>";
 
 ##################################################################################################
 echo "<hr>";
-echo "<h4>2. Criando uma função com Parametro DEFAULT</h4>";
+echo "<h4>4. Criando uma função com Parametro DEFAULT</h4>";
 
 echo "<strong>Exemplo:</strong><p>";
 
@@ -76,7 +76,7 @@ echo "Resultado: Meu nome é João da Silva Sauro!<p>";
 
 ##################################################################################################
 echo "<hr>";
-echo "<h4>2. Criando uma função com Parametro SPLAT OPERATOR \"...\"</h4>";
+echo "<h4>5. Criando uma função com Parametro SPLAT OPERATOR \"...\"</h4>";
 
 echo "<strong>Exemplo:</strong><p>";
 
@@ -93,6 +93,247 @@ echo "Resultado: 3<br>";
 echo "Resultado: 6<p>";
 
 
+##################################################################################################
+echo "<hr>";
+echo "<h4>6. Desempacotar array com SPLAT OPERATOR \"...\"</h4>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "function soma(\$a, \$b, \$c) {<br>";
+echo "echo \$a + \$b + \$c;<br>";
+echo "}<p>";
+
+echo "\$args = array(2,3);<br>";
+echo "soma(1, ...\$args);<p>";
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>7. Função Return</h4>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "function soma(\$x, \$y){<br>";
+echo "\$z = \$x + \$y;<br>";
+echo "return \$z;<br>";
+echo "}<p>";
+
+echo "echo \"5 + 10 = \" . soma(5, 10);<br>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>8. Função Recursiva</h4>";
+
+echo "É uma função que chama ela mesma, precisa ter cuidado para não fazer um loop infinito.<br>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+
+echo "function fatorial(\$numero){<br><br>";
+
+echo "echo \"Numero Atual: \".\$numero.\" \";<br><br>";
+
+echo "if(\$numero <2) {<br>";
+echo "return 1;<br>";
+echo "}else{<br>";
+echo "return (\$numero * fatorial(\$numero-1));<br>";
+echo "}<br>";
+echo "}<p>";
+
+echo "echo \"Resultado: \".fatorial(4);<br>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>9. Tipar variavel dentro de função - TypeHint</h4>";
+
+echo "Podemos determinar o tipo de variavel sera aceita no parametro da função.<br>";
+echo "Se passarmos um valor FLOAT nesse caso ele convertera para inteiro arredondando para cima ou para baixo de acordo com o numero.<br>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "function soma(int \$a, int \$b){<br>";
+echo "return \$a + \$b;<br>";
+echo "}<p>";
+
+echo "soma(1,2);<br>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>10. TypeHint + Splash OPERATOR</h4>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "function soma(int ...\$numeros){<br>";
+echo "echo array_sum(\$numeros);<br>";
+echo "}<p>";
+
+echo "soma(1,2,3,4);<br>";
+echo "soma(1,2,3,4,5,6,7);<br>";
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>11. Determinando que a saida de uma função vai ser inteiro.</h4>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "function soma(float \$a, float \$b) : int {<br>";
+echo "return intval(\$a + \$b);<br>";
+echo "}<p>";
+
+echo "echo soma(1.2,2.5);<br>";
+echo "echo soma(1.8,2.2);<br>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>12. Nullable Types (php 7.1 >=) \"?\".</h4>";
+
+echo "Determina que o valor do parametro ou retorno da função pode receber ou retornar NULO(NULL).<br>";
+echo "Mesmo assim é obrigado a passar um valor ou NULLO.<br>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "function soma(float \$a, float \$b, ?int \$c) : float {<br>";
+echo "return \$a + \$b + \$c;<br>";
+echo "}<p>";
+
+echo "echo soma(5.6,2.6,null);<br>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>13. Strict Types</h4>";
+
+echo "Força o PHP a checar a tipagem de todas as variaveis e não aceitar mais valores não consistentes com o tipo da variavel.<br>";
+
+echo "<strong>Exemplo de declaração:</strong><p>";
+
+echo "declare(strict_types=1);<p>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>14. Passagem de valor por referencia \"&\"</h4>";
+
+echo "Cria um vinculo com o valor da variavel e modifica o valor da variavel de acordo com o que é determinado na função.<br>";
+echo "Sem isso ele só copia o valor da variavel dentro do PARAMETRO e fica sem vinculo com a variavel.<br>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "function somar(\$var1, &\$var2){<br>";
+echo "\$var2 += 2;<br>";
+echo "return \$var1 + \$var2;<br>";
+echo "}<p>";
+
+echo "\$x = 5;<br>";
+echo "echo somar(3, \$x);<br>";
+echo "echo \$x;<p>";
+
+
+function somar($var1, &$var2){
+    $var2 += 2;
+    return $var1 + $var2;
+}
+
+echo "\$x = ". $x = 5;
+echo "<br>";
+echo "Chamando a função = ".somar(3, $x);
+echo "<br>";
+echo "valor de \$x modificado = ".$x;
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>15. Funcção anônima (Closure) - Função sem nome.</h4>";
+
+echo "Cria uma função sem nome que pode ser chamada por uma variavel.<br>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "\$x = function(\$txt){<br>";
+echo "echo (\"Hello \$txt !\");<br>";
+echo "};<p>";
+
+echo "\$x(\"World\");<br>";
+echo "\$x(\"PHP\");<br>";
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>16. Chamando variavel externa dentro de CLOSURE função.</h4>";
+
+echo "Adiciona variavel externa dentro de função.<br>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+
+echo "\$x = 10;<p>";
+
+echo "\$texto = function() use (\$x) { <br>";
+echo "echo \"Olá minha CLOSURE! \$x\";<br>";
+echo "};<p>";
+
+echo "\$texto();<p>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>17. Funcção anônima como parametro de outra função.</h4>";
+
+echo "Determina o valor de uma função CLOSURE e executa a closure em outra função com valor definido.<br>";
+
+echo "<strong>Exemplo 1:</strong><p>";
+
+echo "function teste_closure(callable \$a){<br>";
+echo "\$b = 20;<br>";
+echo "return \$a(\$b);<br>";
+echo "}<p>";
+
+echo "teste_closure(function(\$x){<br>";
+echo "echo(\$x+10);<br>";
+echo "});<p>";
+
+echo "<strong>Exemplo 2:</strong><p>";
+
+echo "function calculaimposto(callable \$formula){<br>";
+echo "\$impostos = [10,22,34,46,58];<p>";
+
+echo "for(\$i =0; \$i < count(\$impostos); \$i++){<br>";
+echo "\$imposto[\$i] = \$formula(\$impostos[\$i]);<br>";
+echo "}<br>";
+echo "return \$impostos;<br>";
+echo "}<p>";
+
+echo "\$resultado = calculaimposto(function(\$valor){<br>";
+echo "return \$valor * 0.1;<br>";
+echo "});<p>";
+
+
+echo "<strong>Exemplo 3:</strong><p>";
+
+
+echo "function add(\$x,\$y){<br>";
+echo "return \$x + \$y();<br>";
+echo "}<p>";
+
+echo "echo add(3, function(){ return 5; });<p>";
+
+
+
+
+function calculaimposto(callable $formula){
+$impostos = [10,22,34,46,58];
+
+for($i =0; $i < count($impostos); $i++){
+$imposto[$i] = $formula($impostos[$i]);
+}
+return $impostos;
+}
+
+echo $resultado = calculaimposto(function($valor){
+return $valor * 0.1;
+});
 
 
 
