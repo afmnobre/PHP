a62d288d193e783a24342e91296db7d708a63d66
afmnobre <lucas_guitar1987@hotmail.com>
2022-10-11 01:20:25 -0300

    modified:   85Bits/Aula-04.php

--- 85Bits/Aula-04.php
+++ 85Bits/Aula-04.php
@@ -53,7 +53,7 @@ echo "echo \$array[null];<br>";
 
 ##################################################################################################
 echo "<hr>";
-echo "<h4>1. Array Bi Dimensionais</h4>";
+echo "<h4>2. Array Bi Dimensionais</h4>";
 
 echo "<strong>Exemplo:</strong><p>";
 
@@ -148,7 +148,180 @@ echo "</td>";
 echo "</tr>";
 echo "</table><p>";
 
+echo "\$shop[0][0].\" custa \".\$shop[0][1].\" e existem \".\$shop[0][2].\" em estoque\";<br>";
+echo "\$shop[1][0].\" custa \".\$shop[1][1].\" e existem \".\$shop[1][2].\" em estoque\";<br>";
+echo "\$shop[2][0].\" custa \".\$shop[2][1].\" e existem \".\$shop[2][2].\" em estoque\";<p>";
 
+echo "camisa custa 9.25 e existem 15 em estoque<br>";
+echo "short custa 19.75 e existem 25 em estoque<br>";
+echo "tênis custa 89.15 e existem 7 em estoque<p>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>3. Array Bi Dimensional Associativo</h4>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+
+echo "\$shop = [<br>";
+echo "[ \"Produto\" => \"camisa\", \"Preco\" => 9.25,   \"Quantidade\" => 15],<br>";
+echo "[ \"Produto\" => \"short\",  \"Preco\" => 19.75,  \"Quantidade\" => 25],<br>";
+echo "[ \"Produto\" => \"tênis\",  \"Preco\" => 89.15,  \"Quantidade\" => 7]<br>";
+echo "];<p>";
+
+echo "<table border=1>";
+echo "<tr><th>0</th><th>1</th><th>2</th></tr>";
+echo "<tr>";
+echo "<td>";
+        echo "<table border=1>";
+        echo "<tr>";
+        echo "<th>";
+        echo "Produto";
+        echo "</th>";
+        echo "<th>";
+        echo "Preco";
+        echo "</td>";
+        echo "<th>";
+        echo "Quantidade";
+        echo "</th>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<td>";
+        echo "\"camisa\"";
+        echo "</td>";
+        echo "<td>";
+        echo "9.25";
+        echo "</td>";
+        echo "<td>";
+        echo "15";
+        echo "</td>";
+        echo "</tr>";
+        echo "</table>";
+echo "</td>";
+echo "<td>";
+        echo "<table border=1>";
+        echo "<tr>";
+        echo "<th>";
+        echo "Produto";
+        echo "</th>";
+        echo "<th>";
+        echo "Preco";
+        echo "</th>";
+        echo "<th>";
+        echo "Quantidade";
+        echo "</th>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<td>";
+        echo "\"short\"";
+        echo "</td>";
+        echo "<td>";
+        echo "19.75";
+        echo "</td>";
+        echo "<td>";
+        echo "25";
+        echo "</td>";
+        echo "</tr>";
+        echo "</table>";
+echo "</td>";
+echo "<td>";
+        echo "<table border=1>";
+        echo "<tr>";
+        echo "<th>";
+        echo "Produto";
+        echo "</th>";
+        echo "<th>";
+        echo "Preco";
+        echo "</th>";
+        echo "<th>";
+        echo "Quantidade";
+        echo "</th>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<td>";
+        echo "\"tênis\"";
+        echo "</td>";
+        echo "<td>";
+        echo "89.15";
+        echo "</td>";
+        echo "<td>";
+        echo "7";
+        echo "</td>";
+        echo "</tr>";
+        echo "</table>";
+echo "</td>";
+echo "</tr>";
+echo "</table><p>";
+
+echo "echo \$shop[0][\"Produto\"].\" custa \".\$shop[0][\"Preco\"].\" e existem \".\$shop[0][\"Quantidade\"].\" em estoque\";<br>";
+echo "echo \$shop[1][\"Produto\"].\" custa \".\$shop[1][\"Preco\"].\" e existem \".\$shop[1][\"Quantidade\"].\" em estoque\";<br>";
+echo "echo \$shop[2][\"Produto\"].\" custa \".\$shop[2][\"Preco\"].\" e existem \".\$shop[2][\"Quantidade\"].\" em estoque\";<p>";
+
+echo "camisa custa 9.25 e existem 15 em estoque<br>";
+echo "short custa 19.75 e existem 25 em estoque<br>";
+echo "tênis custa 89.15 e existem 7 em estoque<p>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>4. Array com Loop - FOR</h4>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "\$array =[ \"Brasil\", \"Argentina\", \"Bolívia\", \"Venezuela\" ];<p>";
+
+echo "for(\$i = 0; \$i < count(\$array); \$i++){<br>";
+echo "echo \$array[\$i];<br>";
+echo "}<br>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>5. Array com FOREACH</h4>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "\$array =[ \"Brasil\", \"Argentina\", \"Bolívia\", \"Venezuela\" ];<p>";
+
+echo "foreach(\$array as \$row){<br>";
+echo "echo \$a;<br>";
+echo "}<p>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>6. Array com FOREACH - CHAVE E VALOR</h4>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "\$array = [10 => \"Brasil\", 20 => \"Argentina\", 30 => \"Bolívia\", 99 => \"Venezuela\"];<p>";
+
+echo "foreach(\$array as \$key => \$pais){<br>";
+echo "echo \"\$key - \$pais\";<br>";
+echo "}<p>";
+
+echo "Resultado<br>";
+echo "10 - Brasil<br>";
+echo "20 - Argentina<br>";
+echo "30 - Bolívia<br>";
+echo "99 - Venezuela<p>";
+
+
+##################################################################################################
+echo "<hr>";
+echo "<h4>7. Array Multidimensional</h4>";
+
+echo "<strong>Exemplo:</strong><p>";
+
+echo "\$pessoas = [<br>";
+echo "[\"nome\" => \"João\", \"cpf\" => 123],<br>";
+echo "[\"nome\" => \"Maria\", \"cpf\" => 452],<br>";
+echo "];<p>";
+
+echo "foreach(\$pessoas as \$pessoa){<br>";
+echo "echo \"nome: \" . \$pessoa[\"nome\"];<br>";
+echo "}<p>";
 
 
 
